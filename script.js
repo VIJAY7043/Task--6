9f7b7f1228499b760a2999bed17fb05ad4058dfa
VijayN <120193561+VIJAYGUVI@users.noreply.github.com>
2023-04-25 14:04:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+//part 2
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i =0; i <11; i++) {
+  new_string += numsArr[i] 
+}
+console.log(new_string);
+//////////////////////////////////////////////////////
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i = 0; i < 11; i++) {
+  new_string += numsArr[i] +","
+
+}
+console.log(new_string);
+/////////////////////////////////////////////////////////
+var new_string = " ";
+for (var i = 11; i => 0; i--) {
+  new_string += numsArr[i] +" "
+}
+console.log(new_string);
+/////////////////////////////////////////////////////////
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (var i = 0; i <=10; i++) {
+ if(numsArr[i] %2 == 0 )
+ {
+ numsArr[i] = "even"
+ }
+}
+console.log(numsArr);
+//////////////////////////////////////////////////////////
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (var i = 0; i <=10; i++) {
+ if(numsArr[i] %2 == 1 )
+ {
+ numsArr[i] = "even"
+ }
+}
+console.log(numsArr);
+///////////////////////////////////////////////////////////
+var numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var sum = 0;
+
+for (var i = 0; i < numsArr.length; i++) {
+  sum += numsArr[i];
+}
+
+console.log(sum);
+//////////////////////////////////////////////////////////////////
+var numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var sum = 0;
+
+for (var i = 0; i < numsArr.length; i++) {
+  if (numsArr[i] % 2 == 0) {
+    sum += numsArr[i];
+  }
+}
+
+console.log(sum);
+////////////////////////////////////////////////////////////////
+var numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var sum = 100;
+
+for (var i = 0; i < numsArr.length; i++) {
+  if (numsArr[i] % 2 !== 0) {
+    sum -= numsArr[i];
+  } else {
+    sum += numsArr[i];
+  }
+}
+
+console.log(sum);
+//////////////////////////////////////////////////////////////////
+var numsArr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]];
+
+for (var i = 0; i < numsArr.length; i++) {
+  console.log(numsArr[i]);
+}
+/////////////////////////////////////////////////////////////////
+var numsArr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]];
+var str_all = "";
+
+for (var i = 0; i < numsArr.length; i++) {
+  var inner_array = numsArr[i];
+  for (var j = 0; j < inner_array.length; j++) {
+    str_all += inner_array[j];
+  }
+}
+
+console.log(str_all);
+////////////////////////////////////////////////////////////////////////////
+part -3
+
+
+
+
+
+
